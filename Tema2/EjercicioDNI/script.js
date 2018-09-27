d12ab48f8d3eacddcc4457a247b913c24c459ba6
jlramos97 <jlramos97@gmail.com>
2018-09-27 14:23:47 +0200

    Corregidos ejercicios js

--- Tema2/EjercicioDNI/script.js
+++ Tema2/EjercicioDNI/script.js
@@ -8,7 +8,7 @@
     var letras = ['T', 'R', 'W', 'A', 'G',
                  'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N',
                  'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
-                 'E', 'T'];
+                 'E', 'T'];                                  
 
     button.onclick = comprobar;
     
