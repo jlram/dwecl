1a7e44e0a9b95d093f2435ee7c87a88386ef4f72
jlramos97 <jlramos97@gmail.com>
2018-10-02 10:15:36 +0200

    omegaoptimizado EjercicioPalindromo

--- Tema2/EjercicioPalindromo/script.js
+++ Tema2/EjercicioPalindromo/script.js
@@ -5,43 +5,33 @@
 
    var paragraph = document.getElementById("paragraph");
    var button = document.getElementById("button");   
-   
-   button.onclick = comprobar;
+
+   button.addEventListener("click", function () {
+       paragraph.textContent = comprobar(document.getElementById("cadena").value);
+   });
     
     /**
      * Usaremos tres cadenas: 
      * - cadenaoriginal, que contiene el texto del input
-     * - cadenaseparada, que es un array de la cadena original sin espacios
+     * - cadenasinespacios, elimina todos los espacios de la cadena original
      * - cadenareves, que hace uso del metodo reverse para dar la vuelta a cadenaseparada
+     *   y join para volver a crear un string.
      * 
      * Una vez hecho esto, con un bucle for comprobaremos si cada uno de los elementos de
      * cadenaseparada y cadenareves son iguales. En caso de serlo, estamos ante un palindromo. 
      */
-    function comprobar() {
+    function comprobar(cadenaoriginal) {
 
         var texto = "La frase " + cadenaoriginal + " no es un palíndromo.";
+        
+        cadenasinespacios = cadenaoriginal.replace(/\s/g,'');
+        var cadenareves = cadenasinespacios.split("").slice().reverse().join("");
 
-        var cadenaoriginal = document.getElementById("cadena").value;
-        var cadenaseparada = cadenaoriginal.replace(/\s/g,'').split(""); // quitar espacios
-        var cadenareves = cadenaseparada.slice().reverse();
-
-        var palindromo = true;
-
-        //Ciclo para comprobar caracter a caracter si  counciden las dos cadenas introducidas
-        //dándole la vuelta para ver si son un palíndromo
-        for (let i = 0; i < cadenaseparada.length && !palindromo; i++) {
-            console.log(cadenaseparada[i], cadenareves[i]);
-            if(cadenaseparada[i] != cadenareves[i]) {
-                palindromo = false;
-            }
-        }
-
-        if (palindromo) {
+        if (cadenasinespacios == cadenareves) {
             var texto = "La frase " + cadenaoriginal + " es un palíndromo.";
         }
 
-        paragraph.textContent = texto;
-
+        return texto;
     }
 
 })();
