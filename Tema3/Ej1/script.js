7dbf52a283df15b06073cbd51792413ad2d2755a
jlramos97 <jlramos97@gmail.com>
2018-10-11 14:43:43 +0200

    Terminado el ejercicio 1 del tema 3

--- Tema3/Ej1/script.js
+++ Tema3/Ej1/script.js
@@ -1,10 +1,36 @@
-(function() {
-    var boton = document.getElementById("BotonAñadir");
-    var texto = document.getElementById("CancionTextInput").value;
+(function (){
 
-    boton.addEventListener("click", manejadorClicBoton);
+    var lista = document.getElementById("listaCanciones");
+    var input = document.getElementById("cancion");
+    var canciones = lista.children;
+
+    var boton = document.getElementById("boton");
+
+    boton.addEventListener('click', function(){
+        
+        if (input.value.trim() == ""){
+            alert("Inserte una canción para poder añadirla.");
+        } else{
+            var existe = false;
+            for (let item of canciones){
+                if (item.textContent=== input.value){
+                    var existe = true;
+                }
+            }
+
+            if(existe) {
+               alert("Esa canción ya existe");     
+            } else {
+                añadeCancion(input.value, lista);
+            }  
+        }
+    });
     
-    function manejadorClicBoton() {
-        alert("El botón ha sido pulsado");
+    function añadeCancion(nombreCancion, lista){
+        var elemento = document.createElement("li");
+        elemento.textContent = nombreCancion;
+        lista.appendChild(elemento);
     }
+
+
 })();
