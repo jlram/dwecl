0396f3b922ece750b244231eb6e55ce81a0aee19
José Luis <jlramos97@gmail.com>
2018-10-18 17:52:37 +0200

    Terminado Ej2FAQ

--- Tema3/Ej2FAQ/script.js
+++ Tema3/Ej2FAQ/script.js
@@ -14,15 +14,29 @@
                 let item = elemento.parentNode.nextElementSibling;
                 item.classList.toggle("oculto");
 
+                elemento.style.backgroundImage = "url('menos.jpg')";
+
+                if (item.classList[0] === "oculto"){
+                    imagen.classList.add("oculto");
+                    elemento.style.backgroundImage = "url('mas.jpg')";    
+                }
+
                 break;
             case 'LI':
+
+                imagen.classList.remove("oculto");
+                
+                /**
+                 * Con esto estoy haciendo un array de los hijos del nodo
+                 * para sacar en qué posición está el elemento seleccionado.
+                 * De esta manera
+                 */
                 var i = Array.from(elemento.parentNode.children).indexOf(elemento);
                 
                 if (elemento.parentNode.id === "ply") {
                     imagen.style.backgroundImage = "url(p" + i + ".jpg)";
-                    console.log(i);
                 } else {
-
+                    imagen.style.backgroundImage = "url(m"+ i + ".jpg)";
                 }
                 
                 break;
