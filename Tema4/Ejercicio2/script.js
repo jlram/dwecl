c4281376538f88188777a5f98ac660732eac330e
jlramos97 <jlramos97@gmail.com>
2018-11-06 09:17:40 +0100

    Iniciado formulario2

---
+++ Tema4/Ejercicio2/script.js
@@ -0,0 +1,18 @@
+/*
+Realiza una una página web que presente un test de preguntas sobre el lenguaje JavaScript.
+· Debe contener al menos 10 preguntas y se deben utilizar los siguientes tipos de elementos de formulario:
+· Más de una respuesta: (varios checkbox)
+· Una sola respuesta de entre varias.
+· Una respuesta a elegir de una lista
+· Una respuesta para insertar en un campo de texto.
+· Una respuesta que sea un comentario que no será evaluable. El comentario contendrá un máximo de 100 caracteres y 
+    debe indicarse en todo momento el número de caracteres que quedan para alcanzar el límite.
+· La página mostrará un botón "Borrar respuestas" y otro "Corrección".
+· El botón Corrección hará que se muestre el número de preguntas contestadas, el porcentaje de aciertos, 
+    el número de aciertos, el número de fallos y el número de preguntas no contestadas, así como el tiempo invertido.
+· Tras el primer intento, el botón "Corrección" se convertirá en "Reintentar".
+· Cuando se hayan acertado todas las preguntas (100% de aciertos) el botón de Corrección se deshabilitará
+*/
+(function (){
+
+})();
