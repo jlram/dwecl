413980589e6441932c54bfb05096ce56f0a7615c
jlramos97 <jlramos97@gmail.com>
2018-11-13 10:05:27 +0100

    Iniciado ejercicio repaso

---
+++ Tema4/EjercicioRepaso/script.js
@@ -0,0 +1,16 @@
+/*
+En c9 https://preview.c9users.io/apsierra/nuevo-ejmplosfaciles/ejercicioViajes.html está el HTML correspondiente a este ejercicio de repaso. Se trata de la página de una agencia de viajes y como verás el HTML usa la misma estructura para cada uno de los viajes (un <div> con clase 'tour' para cada destino. Utilizando el código HTML que se proporciona, debes escribir los ficheros viajes.css y viajes.js  para  hacer que la página se comporte de la siguiente forma:
+
+    Al hacer clic sobre el enlace “Ver fotos” 
+    aparecerán todas las fotos (previamente están ocultas), 
+    al volver a hacer clic desaparecerán.
+    
+    Al modificar el campo "número de noches" de cualquier viaje,
+    se debe modificar automáticamente el precio 
+    (el precio por día está en el span con clase 'tour' correspondiente a ese viaje) .
+
+    Al pulsar el botón de "reserve ahora",
+     desaparecerá dicho botón y aparecerá en su lugar el mensaje:
+      “Llame al 902 30 30 30 para reservar este viaje”.
+*/
+
