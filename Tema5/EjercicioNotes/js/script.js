5191ac8b10b64a9c1d9da077c53ec4455f96cdb1
jlramos97 <jlramos97@gmail.com>
2018-12-11 10:29:30 +0100

    Correccion de sintaxis jQuery

--- Tema5/EjercicioNotes/js/script.js
+++ Tema5/EjercicioNotes/js/script.js
@@ -1,20 +1,26 @@
-$( function() {
+$(function() {
+
+    var draggable = $("#draggable");
+
     $( "#draggable" ).draggable({
-            stack: ".card"
-        }
-    );
+        stack: ".card"
+    });
+    /**
+     * Hace una copia de la variable draggable copiada al principio,
+     * la muestra con un efecto fade in y la convierte en draggable
+     */
+    $(".btn-success").on("click", function(e) {
+        let draggabletemp = draggable.clone().appendTo($("#wrapper")).hide().fadeTo("slow", 1);
+        
+        $(draggabletemp).children(':first-child').text("");
 
-    var button = document.getElementById("button");
-
-    button.addEventListener("click", function() {
-
-        var draggable = $("#draggable");
-
-        let draggabletemp = draggable.clone().appendTo($("#wrapper:first"));
-    
         draggabletemp.draggable({
-                stack: ".card"
-            }
-        );
+            stack: ".card"
+        });
+    });
+
+    $(document).on("click", ".btn-danger", function() {
+        // $(this).closest(".card").remove(); 
+        $(this).parent().remove();
     });
 });
