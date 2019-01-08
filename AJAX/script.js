ee4b158c36e4b6937abae135c574a6a9f813851e
jlramos97 <jlramos97@gmail.com>
2019-01-08 09:17:45 +0000

    Mas o menos lo del ajax

--- AJAX/script.js
+++ AJAX/script.js
@@ -9,14 +9,27 @@
 
     solicitud.open("GET", "https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqbHJhbW9zOTdAZ21haWwuY29tIiwianRpIjoiNzgyY2UxNmMtYTAyNi00OWVjLWFkZmEtZjdlNmE4YjI2MjMzIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE1NDY5MzY4NDMsInVzZXJJZCI6Ijc4MmNlMTZjLWEwMjYtNDllYy1hZGZhLWY3ZTZhOGIyNjIzMyIsInJvbGUiOiIifQ.EGJkktqHpPS7eSiDGhBicIvmo40RkKTQdFqC1FMpYAs");
     solicitud.send(null);
+    
+    var solicitud2 = new XMLHttpRequest();
 
     function muestracontenido() {
         if (solicitud.status == 200) {
-            $.getJSON("datos");
-            var datos = JSON.parse(solicitud.responseText).datos;
-            // console.log(solicitud.responseText);
-            solicitud.open("GET", datos);
-            solicitud.send(null);            
+            let datos = JSON.parse(solicitud.responseText).datos;
+            
+            console.log(solicitud.responseText);
+        
+            solicitud2.open("GET", datos);
+            solicitud2.send(null);     
+            muestradatos();
+            
+        }
+    }
+    
+    function muestradatos() {
+        if(solicitud2.status = 200) {
+                let datos = JSON.parse(solicitud2).response;
+                
+                console.log(datos);
         }
     }
 
