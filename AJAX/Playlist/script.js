654977d34cfad368f29d268e516440a1b98404b6
jlramos97 <jlramos97@gmail.com>
2019-02-07 11:54:33 +0100

    ñ

--- AJAX/Playlist/script.js
+++ AJAX/Playlist/script.js
@@ -4,7 +4,7 @@
 
     var botonLimpia = $("<button>", {
         text: "Borrar",
-        class: 'botonLimpia'
+        class: 'botonLimpia btn btn-light float-right'
     });
 
     /**
@@ -28,7 +28,7 @@
                 $.each(respuesta, function(indice, cancion) {
                     var li = $("<li>", {
                         text: cancion.nombre,
-                        class: 'cancion'
+                        class: 'cancion list-group-item'
                     });
 
                     $("#listaCanciones").append(li);     
@@ -43,13 +43,22 @@
     /**
      * Método para hacer una llamada de AJAX cada 3 segundos
      */
-    setInterval(cargar, 3000);
+    setInterval(cargar, 3000);  
 
     /**
      * Evento del botón de borrar para eliminar la canción 
      */
     $(document).on('click', '.botonLimpia', function() {
         $(this).parent().remove();
+
+        var removeItemTemp = $(this).parent().text();
+        removeItem = removeItemTemp.substring(0, removeItemTemp.length - 6);
+
+        // console.log(removeItem);
+
+        // respuestaCheck = jQuery.grep(respuestaCheck, function(value) {
+        //     return value != removeItem;
+        // });
     });
 
 
